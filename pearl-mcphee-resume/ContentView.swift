eb8aeac4307ef5bf005d2bc7ad07345c5d0f6f00
pearl1991 <pearlmcphee@gmail.com>
2019-09-07 15:47:57 -0400

    Initial Commit

---
+++ pearl-mcphee-resume/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pearl-mcphee-resume
+//
+//  Created by Pearl on 9/7/19.
+//  Copyright © 2019 Pearl. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
